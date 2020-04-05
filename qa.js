c104dbd2451cc006abe2be2f7395189bed0f8f7e
John Slota <jslota17@comcast.net>
2020-04-05 17:31:17 -0500

    Set up readme question

---
+++ qa.js
@@ -0,0 +1,10 @@
+var inquirer = require("inquirer");
+var fs = require("fs");
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "project",
+        message: "What is the name of your project?" 
+    },
+])
