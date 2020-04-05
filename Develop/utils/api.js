4a0e57e9c54d3286c49f70f5cb72de5478b3c9b7
John Slota <jslota17@comcast.net>
2020-04-05 17:09:13 -0500

    NPM install

--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,14 +1,19 @@
+require("dotenv").config();
 const axios = require("axios");
 const api = {
   getUser(username) {
     axios
-    .get()
+    .get('https://github.com/users/${username}',
+    {
+      headers: {"Authorization": 'token ${process.env.GH_TOKEN}'}
+    }
+    )
     .then(response => console.log(response))
     .catch(error => console.log(error))
 
   }
 };
 
-api.getUser();
+api.getUser("Jslota25");
 
 module.exports = api;
