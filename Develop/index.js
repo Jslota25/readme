618fcec9658f60d9585746cc91528d46af038117
John Slota <jslota17@comcast.net>
2020-04-09 22:05:04 -0500

    Added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,47 @@
-const questions = [
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require("fs");
+require("dotenv").config()
+const token = process.env.TOKEN;
 
-];
+//Readme questions
+function questions() {
+    inquirer.prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who contributed to the project?"
+    },
+    {
+        type: "input",
+        name: "technologies",
+        message: "What coding technologies were used in creating the project?"
+    },
+    {
+        type: "input",
+        name: "about",
+        message: "Describe what the project does."
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "What is the license for the project?"
+        },
 
-function writeToFile(fileName, data) {
+    {
+        type: "input",
+        name: "install",
+        message: "How does a user install the project?"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?",  
+    },
+])
 }
-
-function init() {
-
-}
-
-init();
-
-GH_TOKEN = "15b2cc29b0a1987ce172a5d897a590819b45409f"
